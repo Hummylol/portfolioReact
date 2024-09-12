473aad0806436da75ca90c62a00e1a3ed589931d
Hummylol <humaidsadath2044@gmail.com>
2024-09-12 20:45:31 +0530

    modified drawer 0.1

--- frontend/src/components/Drawer/MyDrawer.jsx
+++ frontend/src/components/Drawer/MyDrawer.jsx
@@ -12,11 +12,11 @@ export default function MyDrawer({ addData, dataPercentage, projects }) {
   return (
     <Drawer.Root shouldScaleBackground>
       <Drawer.Trigger asChild>
-        <button className="h-[100%] w-[100%] cursor-default absolute z-[10000000] top-0 left-0"></button>
+        <button className="h-[100%] w-[100%] cursor-default absolute top-0 left-0"></button>
       </Drawer.Trigger>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/80" />
-        <Drawer.Content className="bg-[#060606] text-white flex flex-col rounded-t-[10px] mt-24 fixed bottom-0 left-0 right-0 p-4">
+        <Drawer.Content className="bg-[#060606] text-white flex flex-col z-[10000] rounded-t-[10px] mt-24 fixed bottom-0 left-0 right-0 p-4">
           <div className="mx-auto mt-2 w-12 h-1.5 rounded-full bg-zinc-300 mb-8" />
           <div className="addData text-3xl text-white mb-4">
             {addData}
